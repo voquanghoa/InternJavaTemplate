551006229b37d07dede1a086ceb4ac68cdc39438
Võ Quang Hòa <hvquang@openwt.com>
2019-05-19 00:48:58 +0700

    Reformat file

--- src/main/java/PascalTriangle.java
+++ src/main/java/PascalTriangle.java
@@ -1,18 +1,18 @@
 public class PascalTriangle {
 
-    public int[] generate(int n){
+    public int[] generate(int n) {
         int[] result = new int[n + 1];
 
         result[0] = 1;
 
-        for(int i = 1;i <= n; i++){
+        for (int i = 1; i <= n; i++) {
 
-            for(int j=i; j>=(i+1)/2; j--){
-                result[j] += result[j-1];
+            for (int j = i; j >= (i + 1) / 2; j--) {
+                result[j] += result[j - 1];
             }
 
-            for(int j=1; j<(i+1)/2;j++){
-                result[j] = result[i-j];
+            for (int j = 1; j < (i + 1) / 2; j++) {
+                result[j] = result[i - j];
             }
         }
 
