4aa05c8faf4b62417f2f5b244c877c7d64887066
Võ Quang Hòa <hvquang@openwt.com>
2019-06-11 10:52:37 +0700

    Improve codecov

--- src/test/java/CrazyFiboTest.java
+++ src/test/java/CrazyFiboTest.java
@@ -10,6 +10,13 @@ public class CrazyFiboTest {
         assertEquals(CrazyFibo.next(12345), 23455);
         assertEquals(CrazyFibo.next(76528), 65288);
     }
+
+    @Test
+    public void test_calculate(){
+        assertEquals(CrazyFibo.calculate(12345, 1, 2, 3, 4, 5), 0);
+        assertEquals(CrazyFibo.calculate(2222, 2, 4, 3, 4, 5), 5);
+        assertEquals(CrazyFibo.calculate(3, 2, 4, 3, 4, 5), 4);
+    }
 }
 
 
