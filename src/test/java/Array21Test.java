6580943da39c4d7ead835325ac618d58c72fa716
Võ Quang Hòa <voquanghoa@gmail.com>
2019-06-19 09:16:34 +0700

    Update Array21Test.java

--- src/test/java/Array21Test.java
+++ src/test/java/Array21Test.java
@@ -6,16 +6,19 @@ public class Array21Test {
 
         @Test
         public void test_calculateDeterminant(){
-            int [][]arr = {{-1,2,3},{2,-1,4},{0,-3,2}};
+            int [][]arr = {
+                    {-1,        2,      3},
+                    {2,         -1,     4},
+                    {0,         -3,     2}};
             assertEquals(new Array21().calculateDeterminant(arr),-36.0,0.001);
         }
 
         @Test
         public void test_calculateDeterminant2(){
             int [][]arr = {
-                    {1,	9,	3,	4},
-                    {5,	6,	7,	9},
-                    {3,	10,	11,	12},
+                    {1,	        9,	3,	4},
+                    {5,	        6,	7,	9},
+                    {3,	        10,	11,	12},
                     {13,	2,	5,	3}
             };
 
@@ -25,12 +28,12 @@ public class Array21Test {
         @Test
         public void test_calculateDeterminant3(){
             int [][]arr = {
-                    {4,	11,	28,	3,	9,	1},
-                    {2,	2,	4,	28,	2,	14},
-                    {6,	3,	9,	17,	18,	7},
-                    {3,	3,	1,	16,	6,	15},
-                    {6,	5,	3,	2,	9,	23},
-                    {1,	6,	11,	2,	19,	16}
+                    {4,	        11,	28,	3,	9,	1},
+                    {2,	        2,	4,	28,	2,	14},
+                    {6,	        3,	9,	17,	18,	7},
+                    {3,	        3,	1,	16,	6,	15},
+                    {6,	        5,	3,	2,	9,	23},
+                    {1,	        6,	11,	2,	19,	16}
             };
 
             assertEquals(new Array21().calculateDeterminant(arr),-2399168,0.001);
@@ -40,7 +43,7 @@ public class Array21Test {
         public void test_calculateDeterminant4(){
             int [][]arr = {
                     {30,	2,	69,	35,	66},
-                    {8,	32,	52,	59,	11},
+                    {8,	        32,	52,	59,	11},
                     {87,	41,	97,	39,	79},
                     {15,	94,	77,	25,	98},
                     {36,	23,	20,	8,	83}
